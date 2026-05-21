f9838b0d434532a8a2d02d69ccd895b08a184303
ravi-anudip <ravi.patel@anudip.org>
2026-05-21 15:28:38 +0530

    21/05/26

--- EXPRESS/Day12/Getting Data/server.js
+++ EXPRESS/Day12/Getting Data/server.js
@@ -1,6 +1,20 @@
 let express = require('express');
 const app = express();
+const db_conn = require('./db_conn.js');
+var ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
 
 app.get('/listuser', (req, res) => {
-  db_conn.query('select * from user')
+  db_conn.query('select * from user', (err, result, fieldlist) => { 
+    if (err) throw err;
+    else {
+      // console.log(result);
+      // return res.send("result is printed at console");
+      res.render('UserList',{data:result});
+    }
+  });
 });
+
+app.listen(8080, () => console.log("server running at port no 8080"));
